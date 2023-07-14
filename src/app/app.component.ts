419ed9ed1800ae2068c1504e8f2782ff2ad84fc5
toavina <toavinathefirst@gmail.com>
2023-07-14 23:45:41 +0200

    ajout de home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,9 @@ import { HomeComponent } from './home/home.component';
     <header class="brand-name">
       <img src="/assets/logo.svg" alt="logo" aria-hidden="true"class="brand-logo">
     </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
   </main>`,
   styleUrls: ['./app.component.css'],
 })
