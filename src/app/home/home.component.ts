b2394adbdc7b6e2974743938b832191a9eca7b2e
toavina <toavinathefirst@gmail.com>
2023-07-16 23:47:31 +0200

    binding to a components template and @input

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,HousingLocationComponent],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
       <form>
-        <input type="search" name="search" placeholder="filter by city">
+        <input type="search" name="search" placeholder="Filter by city" />
         <button type="button" class="primary">Search</button>
       </form>
     </section>
     <section class="results">
-      <app-housing-location></app-housing-location>
+      <app-housing-location
+        [housingLocation]="housingLocation"
+        ]
+      ></app-housing-location>
     </section>
   `,
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa ';
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
