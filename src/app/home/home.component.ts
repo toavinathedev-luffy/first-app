cf576a1d44f5539ae74162dc237028662a7b5131
toavina <toavinathefirst@gmail.com>
2023-07-16 23:07:02 +0200

    create interface housingLocation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,HousingLocationComponent],
   template: `
     <section>
       <form>
         <input type="search" name="search" placeholder="filter by city">
         <button type="button" class="primary">Search</button>
       </form>
+    </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css']
