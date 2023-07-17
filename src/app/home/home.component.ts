097f2ab25938db55c1dcb3b094648dd250fcecd8
toavina <toavinathefirst@gmail.com>
2023-07-17 14:30:45 +0200

    *ngfor for listing housinglocation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,6 +15,7 @@ import { HousingLocation } from '../housing-location-interf';
     </section>
     <section class="results">
       <app-housing-location
+        *ngFor="let housingLocation of housingLocationList"
         [housingLocation]="housingLocation"
       ></app-housing-location>
     </section>
@@ -23,14 +24,76 @@ import { HousingLocation } from '../housing-location-interf';
 })
 export class HomeComponent {
   readonly baseUrl = '../assets';
-  housingLocation: HousingLocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/luffy.png`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+  housingLocationList: HousingLocation[] = [
+    {
+      id: 0,
+      name: 'Acne Fresh start housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: `${this.baseUrl}/images (8).jpeg`,
+      availableUnits: 4,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 1,
+      name: 'A113 Transtional Housing',
+      city: 'santa Monica',
+      state: 'CA',
+      photo: `${this.baseUrl}/images (9).jpeg`,
+      availableUnits: 0,
+      wifi: false,
+      laundry: true,
+    },
+    {
+      id: 2,
+      name: 'Warm Beds Housing support',
+      city: 'Juneau',
+      state: 'AK',
+      photo: `${this.baseUrl}/images (10).jpeg`,
+      availableUnits: 1,
+      wifi: false,
+      laundry: false,
+    },
+    {
+      id: 3,
+      name: 'Homestaedy Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: `${this.baseUrl}/images (11).jpeg`,
+      availableUnits: 1,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 4,
+      name: 'Happy Homes Group',
+      city: 'Gary',
+      state: 'IN',
+      photo: `${this.baseUrl}/images (12).jpeg`,
+      availableUnits: 1,
+      wifi: false,
+      laundry: false,
+    },
+    {
+      id: 5,
+      name: 'Hopeful Appartement Group',
+      city: 'Oakland',
+      state: 'CA',
+      photo: `${this.baseUrl}/téléchargement (26).jpeg`,
+      availableUnits: 2,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 6,
+      name: 'Warm Beds Housing support',
+      city: 'Copakabama',
+      state: 'AK',
+      photo: `${this.baseUrl}/téléchargement (27).jpeg`,
+      availableUnits: 3,
+      wifi: false,
+      laundry: false,
+    },
+  ];
 }
