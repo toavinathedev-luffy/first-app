beb17deccd2108284ce032f524ea028fb66efa5e
toavina <toavinathefirst@gmail.com>
2023-07-21 12:04:46 +0200

    []

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -14,7 +14,7 @@ export class HousingService {
     id: number
   ): Promise<HousingLocation | undefined> {
     const data = await fetch(`${this.url}/${id}`);
-    return (await data.json()) ?? {};
+    return (await data.json()) ?? [];
   }
   submitApplication(firstName: string, lastName: string, email: string) {
     console.log(firstName, lastName, email);
